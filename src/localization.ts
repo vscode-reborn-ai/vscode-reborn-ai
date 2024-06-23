98229be2f3d63767d8901a5712dbd279c644a5a1
Chris Hayes <6013871+Christopher-Hayes@users.noreply.github.com>
2024-06-23 00:27:48 -0400

    :recycle: Refactor: move localization files into folder

--- src/localization.ts
+++ src/localization.ts
@@ -1,28 +1,29 @@
-import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
+import { readFileIfExists } from "./helpers";
+const fs = vscode.workspace.fs;
 
 export async function loadTranslations(extensionPath: string): Promise<Record<string, string>> {
   // Load default package.nls.json file
   const defaultTranslationsPath = path.join(extensionPath, 'package.nls.json');
-  const defaultTranslations = JSON.parse(await fs.promises.readFile(defaultTranslationsPath, 'utf8'));
+  const defaultTranslations = JSON.parse(await readFileIfExists(defaultTranslationsPath) ?? '');
 
   // Detect the current locale
   const locale = vscode.env.language;
 
   // Load the locale-specific translations file if it exists
-  const localeTranslationsPath = path.join(extensionPath, '', `package.nls.${locale}.json`);
-  let localeTranslations = {};
+  const localeTranslationFile = vscode.Uri.file(`${extensionPath}/localization/package.nls.${locale}.json`);
+  let translatedContent = {};
 
   try {
-    await fs.promises.access(localeTranslationsPath, fs.constants.F_OK);
-    localeTranslations = JSON.parse(await fs.promises.readFile(localeTranslationsPath, 'utf8'));
+    translatedContent = JSON.parse(await readFileIfExists(localeTranslationFile.path) ?? '');
+    console.debug(`[Reborn AI] Loaded locale translations for ${locale}.`, translatedContent);
   } catch (err: any) {
     console.error(`[Reborn AI] Locale translations not found: ${err.message}`);
   }
 
   // Merge default translations with locale-specific translations
-  const combinedTranslations = { ...defaultTranslations, ...localeTranslations };
+  const combinedTranslations = { ...defaultTranslations, ...translatedContent };
 
   // Iterate through the keys and for each period in the key, create a nested object and move the value to the leaf
   for (const key of Object.keys(combinedTranslations)) {
