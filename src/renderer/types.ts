e0db362a07b2709f393bdd98dc6bb09fd9e33a85
Chris Hayes <chris+git@hayes.software>
2024-07-19 19:27:14 -0400

    :recycle: Refacotr: update types to reflect version rename

--- src/renderer/types.ts
+++ src/renderer/types.ts
@@ -315,7 +315,7 @@ export interface ExtensionSettings {
   renameTabTitles: boolean;
   showAllModels: boolean;
   manualModelInput: boolean;
-  apiVersion: string;
+  azureApiVersion: string;
 }
 
 export const DEFAULT_EXTENSION_SETTINGS: ExtensionSettings = {
@@ -367,5 +367,5 @@ export const DEFAULT_EXTENSION_SETTINGS: ExtensionSettings = {
   renameTabTitles: true,
   showAllModels: false,
   manualModelInput: false,
-  apiVersion: "2024-02-01"
+  azureApiVersion: "2024-02-01"
 };
